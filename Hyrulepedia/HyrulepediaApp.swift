f9769d37e4af7832886d7b31053971217554e629
JaviDev <javi.apps.developer@gmail.com>
2026-01-01 22:48:40 +0100

    New home screen

--- Hyrulepedia/HyrulepediaApp.swift
+++ Hyrulepedia/HyrulepediaApp.swift
@@ -8,3 +8,18 @@ struct HyrulepediaApp: App {
         }
     }
 }
+
+extension Color {
+    static let customWhite = Color(red: 244/255,
+                                   green: 250/255,
+                                   blue: 250/255)
+    static let customWhiteGlow = Color(red: 100/255,
+                                   green: 200/255,
+                                   blue: 255/255)
+    static let customGold = Color(red: 185/255,
+                                   green: 160/255,
+                                   blue: 102/255)
+    static let mintGreen = Color(red: 111/255,
+                                 green: 212/255,
+                                 blue: 156/255)
+}
--- Hyrulepedia/Presentation/Home/HyrulepediaGameSelectorScreen.swift
+++ Hyrulepedia/Presentation/Home/HyrulepediaGameSelectorScreen.swift
@@ -7,15 +7,61 @@ struct HyrulepediaGameSelectorScreen: View {
         ZStack {
             BackgroundView()
             VStack {
-                Text("Home Game Selector")
-                    .foregroundStyle(.white)
-                Button("Go to Breath of the Wild") {
-                    coordinator.push(.breathOfTheWild)
+                Image("app-title-tan")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .padding(.bottom, 32)
+                Text("Choose game")
+                    .font(.custom("HyliaSerifBeta-Regular", size: 35))
+                    .shadow(color: .customGold.opacity(0.6), radius: 3)
+                    .shadow(color: .customGold.opacity(0.4), radius: 5)
+                HStack(spacing: 24) {
+                    VStack {
+                        Image("botw-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                            .onTapGesture {
+                                coordinator.push(.breathOfTheWild)
+                            }
+                    }
+                    VStack {
+                        Image("totk-logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                            .onTapGesture {
+                                coordinator.push(.tearsOfTheKingdom)
+                            }
+                    }
                 }
-                Button("Go to Tears of the Kingdom") {
-                    coordinator.push(.tearsOfTheKingdom)
+                Spacer()
+                HStack {
+                    Text("API by")
+                    Link("gadhagod", destination: URL(string: "https://gadhagod.github.io/Hyrule-Compendium-API/#/")!)
+                        .italic()
+                        .bold()
+                        .underline()
                 }
+                HStack {
+                    Text("Resources by")
+                    Link("Hunter Paramore", destination: URL(string: "https://admiregd.com/zelda-breath-of-the-wild-ui-kit")!)
+                        .italic()
+                        .bold()
+                        .underline()
+                }
+                HStack {
+                    Text("Desing inspired by")
+                    Link("Basti UI Zeld'App", destination: URL(string: "https://www.figma.com/community/file/1247926937471212468/bastiui-zeldapp-zelda-botw-totk-iphone-app")!)
+                        .italic()
+                        .bold()
+                        .underline()
+                }
+                
             }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .foregroundStyle(Color.customWhite)
         }
     }
 }
@@ -47,4 +93,3 @@ private extension HyrulepediaGameSelectorScreen {
 #Preview {
     HyrulepediaGameSelectorScreen()
 }
-
